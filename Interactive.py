def2d11d930c66fb5a65f67d1f8f96d1690dc80d
Ben Sehnert <bps53@protonmail.com>
2020-10-16 12:41:46 -0400

    debugged prompt functions in interactive. lot of nesting and while loops. I dont like that- took a lot out still works. for the most part
    defineing some new functions
    for identifier validation. need to
    learn to use re module for
    matching with regex.
    that and debug with vscode.

--- Interactive.py
+++ Interactive.py
@@ -6,8 +6,8 @@ should I design a better way to do this
 
 TODO also start using git'''
 
+import re
 from Misc_Functions import list_to_str
-from Class_Dict import Class_Dict
 import argparse
 import sys
 import os
@@ -33,35 +33,38 @@ parser.add_argument("-e", "--exporting", type=str, metavar='', choices=['tgz', '
 parser.add_argument("-i", "--interactive", help="Run the program in interactive mode!", action= "store_true" , required = False)
 
 
-#read arguments passed to the console.
 args = parser.parse_args()
 
-#global variable for all functions to access.
+#Global that holds all classes parsed via application use.
 container = {}
 
 def interactive_main():
     """What a user sees when they fire up the program in -i mode"""
-    print("\nWelcome to class generator interactive mode!\n")
+    print("\nWelcome to Class Generator interactive mode!\n")
     help = "Default mode- Sequentially list your classes in the desired 'inline' format.\n\
 example_class : attr1, attr2, attr3 : method1, method.\n\
 Prompt Mode- Build your class or classes with the help of an interactive prompt.\n\
 Note that you can leave any time by entering q, e, quit or exit.\n\
 Type d for default mode or p for Prompt mode, h repeats these instructions."
     print(help)
-    while (response := input()) and response not in ("quit","exit","e","q"):
+    response = input()
+    while (response not in ("quit","exit","e","q")):
         if response in ("h", "help"):
             print(help)
-            continue
+            response = input()
         elif response in ("d","default","default mode"):
-            if answer := default_mode():
-                container.update(answer)
+            default_mode()
+            # if default_mode():
+            #     print("You picked in ")            
+                #answer = default_mode()?
+
+                # container.update(answer)
         elif response in ("p","prompt","prompt mode"):
-            if answer := prompt_mode():
-                container.update(answer)
-        #Fallthrough/ default- input provided was not an option. 
+            prompt_mode()
+            # if answer := prompt_mode():
+            #     container.update(answer)
         else:
-            print("Did not recoginize your input. Please try again.")
-            continue
+            response = input("Did not recoginize your input. Please try again: ")
 
 def default_mode():
     """
@@ -77,16 +80,39 @@ methods types with a prepended SM/CM for static or class methods\n\
 leaving attributes or methods sections will result in classes without respective attributes or method\n\
 type c, d, continue or done to exit this mode\n"
     print(help)
-    while (response := input()) and response not in ("quit","exit","e","q"):
-        lines.append(response)
+    response = input()
+    while response not in ("quit","exit","e","q"):
         if response.lower() in ("c","d","continue","done"):
+            print("Were done parsing inline class dicts")
+            print(lines)
             #gonna have to botch the class_dict design AGAin
-            inline = response.split(":")
-            print(*inline)
+            # inline = response.split(":")
+            # print(*inline)
             #cont.update(Class_Dict.to_dict(response))
         elif response.lower() in ("q","e","quit","exit"):
             sys.exit(1)
+        response = input()
+        if validate_inline(response):
+            lines.append(response)
+            continue
 
+
+def validate_classes(classes):
+    for item in classes:
+        if item[0] == '0':
+            print("first character of identifier cannot be 0")
+        for i in item:
+            if i not in re.match('[a-zA-Z_][a-zA-Z0-9_]*'):
+                return "a class provided contained an illegal character."
+
+
+
+def validate_inline(inline):
+    classes = inline.split(":")[0]
+    if len(classes):
+        validate_classes(classes)
+    else:
+        print("cannot define a class with no name. ")
 
 def prompt_mode():
     """
@@ -107,14 +133,15 @@ def get_classes(classes = []):
 you have enumerated all class names in your project\n\
 Key- move forward : [c,d,continue,done], help : [h,help], leave prompt : [q,e,quit,exit]"
     print("List the names of classes to be generated. When done enter 'continue','done','c','q'")
-    while (cls := input()) not in ("q","e","quit","exit"):
-        if cls in ("c","d","continue","done"):
+    classes = input()
+    while classes not in ("q","e","quit","exit"):
+        if classes in ("c","d","continue","done"):
             if get_confirmation(classes, member="class"):
                 return classes
-            continue
+            classes = input()
         elif cls in ("h","help"):
             print(help)
-            continue
+            classes = input()
         #assuming user didnt enter a recognized keyword, appends the response to classes
         #need to parse cls to check compliance with class naming rules/conventions.
         classes.append(cls)
@@ -136,34 +163,59 @@ returns a list of attributes
     """
     container = []
     help = "key- next step : [c,d,continue,done], help :\
-[h,help], exit prompt: [q,e,quit,exit]\n\n"
+[h,help], exit prompt: [q,e,quit,exit]\n\n\
+Otherwise, type names for your attribute, one per line, and type <enter> to move to next line/ attribute"
     print("list {} for class named {} : ".format(member, cls))
-    while (response := input()) not in ("q","e","quit","exit"):
-        #need to parse cls to check compliance with class naming rules/conventions.
+    response = input()
+    while response not in ("q","e","quit","exit"):
+        #this i suspect will be a problem area...
         if response in ("c","d","continue","done"):
             if get_confirmation(container, member=member):
-                return list_to_str(container)
+                return container
             else:
                 continue
         elif response in ("h","help"):
             print(help)
+            response = input()
             continue
         #assuming user didnt enter a recognized keyword, appends the response to classes
         container.append(response)
+        response = input()
     #kill the program if the loop was exited. user wants to quit.
     sys.exit(1)
 
 def get_confirmation(items, member=None):
     """Asks user if they are satisfied with current list"""
+    print("\n")
     for elements in items:
         print(elements)
     print("satisfied with your current {} list?(y/n)".format(member))
     while True:
         ans = input()
         if ans in ("y","yes"):
-            return items
+            return True
         elif ans in ("n","no"):
-            return False
+            if abort_state():
+                if get_confirmation(items, member):
+                    return True
+
+
+def abort_state():
+    print("What would you like to do ? >>> 1) complete this entry 2) abort this generation/ close the application")
+    response = input()
+    if response == "1":
+        return True
+    elif response == "2":
+        print("Careful: proceed with caution!")
+        print("Executing this command will result in erasure of classes specified in this session.")
+        choice = input("and no classes will be generated! are you sure you want to do this? (y/n)")
+        if choice.lower() in ("y","yes","yeah","yea","ya"):
+            sys.exit(1)
+        else:
+            return True
+    
+
+
 
 
 #Dev notes: 3/29/2020 @ 5:24pm EST
@@ -179,4 +231,7 @@ def get_confirmation(items, member=None):
 
 #test the first guy at least...
 ####expected one or more argument?
+# interactive_main()
+# print(get_confirmation(["bisk","chalp","skone","POADES"]))
+# print(get_data("Shaat"))
 interactive_main()
