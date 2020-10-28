25e244a98805d5d4871bf20dcf7d91b856a97bf7
Ben Sehnert <bps53@protonmail.com>
2020-10-28 16:57:54 -0400

    Added documentation, refactored a little. added a single test case.

--- Parser/Inline.py
+++ Parser/Inline.py
@@ -1,7 +1,55 @@
-class Inline:
-    # not worried about validation here because this is the user input, its parsed and rejected until a valid one is submitted.
+'''
+Programmer: Ben Sehnert
+Program: inline class for ClassGen program
+Date: 10/28/2020
+Module level docstring: implements the Inline class
+'''
 
-    def __init__(self, classes=None, attributes=None, methods=None, global_testing=None, global_exporting=None):
+
+class Inline:
+    '''
+Class Level Docstring: this is a class that repersents inline class
+specifications.
+
+constructor: creates an instance of the Inline class. performs cleansing to
+strip white space from fields, which is noise in the classGen mini language.
+
+    Parameters:
+        -classes = None : repersents the class to be generated's identifier(s)
+        -attributes = None : repersents the class to be generated's attribute(s)
+        -methods = None : repersents the class to be generated's method(s)
+        -global_testing = False : should unit tests be generated for all files
+        -global_export = None : What should be done to the project after generation.
+        if the field is not set to None, it will be a set of arguments/directives (what to do next?)
+
+    Return values: None
+
+    Side-effects: creates an Inline object in the scope of the 
+    invokation/call to the constructor. Memory is consumed while
+    the object is in use.
+
+    Exceptions: Unknown at this point.
+
+Alternative Constructor: class method that generates the 
+inline from (validated) user input.
+
+    Parameters:
+        -inline : str - the inputted inline class spec, of the format:
+        'class : attribute(s), : method(s), (switches: -t -e{arg})'
+
+    Return values: a reference to a freshly generated Inline instance,
+    matching the specifications of the inline argument provided.
+
+    Side-effects: creates an Inline object in the scope of the
+    invokation/call to the constructor. Memory is consumed whule
+    the object is in use.
+
+    Exceptions: Unknown at this point.
+    '''
+
+    version = 2.1
+
+    def __init__(self, classes=None, attributes=None, methods=None, global_testing=False, global_exporting=None):
         def cleanse(items: str):
             def clean(x): return x.strip()
             items = map(clean, items.split(","))
@@ -27,5 +75,8 @@ class Inline:
 
 if __name__ == "__main__":
     new_line = Inline.from_inline(
-        "classA : attr1, attr2 : method1, method2 -t -e{us,ts,er}")
-    print(new_line.global_testing)
+        "classA : attr1, attr2, attr3, attr4 : method1, method2 -t -e{us,ts,er}")
+    print(new_line.attributes)
+
+    # future test case- if a field (attr or method) is not provided,
+    # the classes attr or method field should be left as None.
