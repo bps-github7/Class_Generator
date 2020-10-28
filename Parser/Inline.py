c2a6c700be3a993f3a7d77be2b47a569c4a93984
Ben Sehnert <bps53@protonmail.com>
2020-10-28 16:37:03 -0400

    Did a neat little functional programming trick to get inline fields to cleanse themselves.
    This class i think is good to use else where in the program.
    i think you should rethink/redesign
    architecture first. but also
    would not hurt to unit test this class
    to avoid complications/confusiond
    down the line

--- Parser/Inline.py
+++ Parser/Inline.py
@@ -1,32 +1,31 @@
 class Inline:
     # not worried about validation here because this is the user input, its parsed and rejected until a valid one is submitted.
 
-    def __init__(self, classes=None, attributes=None, methods=None, testing=None, exporting=None, parents=object):
-        self.classes = classes
-        self.attributes = attributes
-        self.methods = methods
-        self.testing = testing
-        self.exporting = exporting
-        self.parents = parents
+    def __init__(self, classes=None, attributes=None, methods=None, global_testing=None, global_exporting=None):
+        def cleanse(items: str):
+            def clean(x): return x.strip()
+            items = map(clean, items.split(","))
+            return ",".join(items)
+        self.classes = cleanse(classes.strip())
+        self.attributes = cleanse(attributes.strip())
+        self.methods = cleanse(methods.strip())
+        self.global_testing = global_testing
+        self.global_exporting = global_exporting
 
-    # alternative constructor
     @classmethod
     def from_inline(cls, inline: str):
-        return Inline(*inline.split(":"))
-
-    # need to find a way to work this into an instance method- can it be done. why yes of course!
-    def testing(cls, inline: str):
-        if inline.count("-t"):
-            return True
-
-    def export(cls, inline: str):
-        if inline.count("-e"):
-            return inline.split("-e")[1]
+        def testing(x): return True if x.count("-t") else False
+        def exporting(x): return inline.split("-e")[1] if x.count("-e") else 0
+        new_line = inline.split(":")
+        modified_methods = new_line[2].split("-t")[0]
+        return Inline(classes=new_line[0],
+                      attributes=new_line[1],
+                      methods=modified_methods,
+                      global_testing=testing(inline),
+                      global_exporting=exporting(inline))
 
 
 if __name__ == "__main__":
     new_line = Inline.from_inline(
-        "classA : attr1, attr2 : method1, method2 -t")
-    if new_line.methods.count("-"):
-        switch = new_line.methods.split("-")[1]
-        print(switch)
+        "classA : attr1, attr2 : method1, method2 -t -e{us,ts,er}")
+    print(new_line.global_testing)
