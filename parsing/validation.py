b3e9f18fb0b2dac2c989fb34c2849a39f0bc7f99
Ben Sehnert <bps53@protonmail.com>
2021-01-07 23:57:54 -0500

    Making progress w. validation. seems to be some small problem w return value lost or ambiguous in callstack. debug tomorrow.
    crossing out that, we need to modify
    constructor for inline and fix breaking
    changes from that, then
    basic inlines will be auto validated,
    then onto multi, inheritance, and packaging.

--- parsing/validation.py
+++ parsing/validation.py
@@ -6,10 +6,26 @@ Date: 1/7/2021
 
 utility functions for validating user input.
 """
-# import sys
+import sys
+# import os
+
+sys.path.insert(0, "C:\\Users\\Ben\\VsCode\\python\\classgenerator")
 import keyword
+from parsing.inline import Inline
 
 def ask_case(item, item_type="class"):
+    """
+    In the case that the user has set the preference of case correction:"ask"
+    checks if field has correct case according to pep8, in the case it is not
+    if the user agrees, modifies the identifier with the correct case.
+
+    item [str] - the real time value of the identifier.
+    item_type="class" [str] - can be class or field.
+
+    Returns:
+        item [str]: the same item passed in, stripped of whitespace,
+        and with correct case if requested. 
+    """
     if item_type == "class":
         if item.istitle():
             return item.strip()
@@ -26,13 +42,25 @@ def ask_case(item, item_type="class"):
             return item.strip()
 
 def case_prompt(item, item_type="class"):
+    """
+    Prompts user whether or not they want to correct errors
+    in how their identifier is cased.
+
+    item [str] - the real time value of the identifier.
+    item_type="class" [str] - can be class or field.
+
+    returns int:
+    1 - user afirms the the mistake should be corrected.
+    0 - user denies the mistake should be corrected.
+    
+    """
     while True:
         if item_type == "class":
             response = input(f"your class name {item} is not capitalized.\n\
-        This violates PEP8 guidelines- should this be corrected (y/n)?")
+        This violates PEP8 guidelines- should this be corrected (y/n)?\n")
         else:
             response = input(f"your attribute or method name {item} is not lowercase.\n\
-        This violates PEP8 guidelines- should this be corrected (y/n)")
+        This violates PEP8 guidelines- should this be corrected (y/n)\n")
         if response in ("y", "yes"):
             return 1
         return 0
@@ -59,6 +87,15 @@ def case_check(item, item_type="class", preferences="ask"):
     based on user preferences, set in rc file:
     either coerce, ask user or do nothing about
     incorrectly cased identifiers.
+
+    
+    item [str] - the real time value of the identifier.
+    item_type="class" [str] - can be class or field.
+    preference="ask" [str] - what the user prefers
+    regarding correction of case.
+
+    return [str] - the modified value of the identifier,
+    according to what the user requested.
     """
     if preferences == "ask":
         return ask_case(item, item_type=item_type)
@@ -68,7 +105,13 @@ def case_check(item, item_type="class", preferences="ask"):
         return coerce_case(item, item_type=item_type)
 
 def is_identifier(ident: str) -> bool:
-    """Determines if string is valid Python identifier."""
+    """Determines if string is valid Python identifier.
+    
+    ident [str] - the real time value of identifier
+
+    returns [bool]- True or False based on whether
+    the ident argument is an identifier.
+    """
 
     if not isinstance(ident, str):
         raise TypeError("expected str, but got {!r}".format(type(ident)))
@@ -94,18 +137,97 @@ def basic_validate_members(items, item_type="class"):
     container = []
     for item in items:
         if is_identifier(item):
-            container.append(case_check(item))
+            container.append(case_check(item, item_type=item_type))
+    print(container)
     return container
 
 
-def basic_validate(items, item_type="class"):
-    """validates basic inline-
-        1. does it have Classes
-        2. does it follow syntax
-        3. does it fail basic validation member tests?
+def basic_validate(inline : str):
     """
-    return NotImplemented
+    """
+    inline = inline.split(":")
+    #fall through test - cant generate class if no class names are provided
+    if inline[0].strip():
+        classes = basic_validate_members((inline[0].strip()).split(","))
+        if len(inline) > 1:
+            pass
+            if inline[1].strip():
+                attributes = basic_validate_members((inline[1].strip()).split(","), item_type="field")
+                if len(inline) > 2 and inline[2].strip():
+                    methods = basic_validate_members((inline[2].strip()).split(","), item_type="field")
+                    # return Inline(classes, attributes, methods)
+                else:
+                    if missing_field(type="method"):
+                        print("missing methods")
+                        # print(f"{clas} : {}")
+                        # inline(classes, attributes, None)
+            else:
+                # redundant but prevents methods from being
+                #  undefined in if below in same else block.
+                if len(inline) > 2 and inline[2].strip():
+                    methods = basic_validate_members(inline[2].strip(), item_type="field")
+                else:
+                    if missing_field(type="none"):
+                        print("missing both attr and methods")
+                        # return Inline(classes, None, None)
+                if missing_field(type="attribute"):
+                    print("missing attributes")
+                    # return Inline(classes, None, methods)
+        else:
+            # make an Inline w/ niether fields if user accepts that.
+            if missing_field(type="none"):
+                print("missing both attributes and methods.")
+                # return Inline(classes, None, None)
+            else:
+                return 0
+    else:
+        return missing_field()
 
+def missing_field(type="class"):
+    """
+    prints appropriate message based on one of 3 missing fields.
+
+    field [str] - the real time value of inline field.
+    type="class" [str] - what type of field dealt w/ determines error message.
+
+    return [int] -
+    1 - proceed with parsing
+    0 - do not proceed.
+    """
+    if type == "class":
+        print("Error: cannot make class with no class name.")
+        return 0
+    elif type == "attribute":
+        if continue_prompt():
+            return 1
+        else:
+            return 0
+    elif type == "method":
+        if continue_prompt(field_type="method"):
+            return 1
+        else:
+            return 0
+    elif type == "none":
+        if continue_prompt(field_type="none"):
+            return 1
+        else:
+            return 0
+
+        
+def continue_prompt(field_type="attribute"):
+    if field_type == "none":
+        message = "the inline provided has neither attributes or methods"
+    else:
+        message = f"the inline provided has no {field_type}s."
+    while True:
+        print(message)
+        response = input("proceed with generation (y/n)?")
+        if response in ("y", "yes"):
+            return 1
+        elif response in ("n", "no"):
+            return 0
+        else:
+            print("sorry, didnt understand your response. valid: y or n")
 
 def multiple_validate(items, item_type="class"):
     """
@@ -117,21 +239,13 @@ def inheritance_validate(items, item_type="class"):
     """
     return NotImplemented
 
-# def validate_nested(line):
-#     '''carefully design packaging structure as to not interfere with the already established inheritance syntax!'''
-#     # ideas: remodel inheritance language so that -> marks inheritance </> marks package- <p: package_name c: ( class spec)>
-#     # nested package : <p: c:(...) <p  <p: c: > <p: c: > <p: c: > >   > this would mean the space after class specs denotes nested packages, and must be left blank otherwise.
-#     # rootward nesting: should advise against it. suggest to users they start at their project root and work downwards.
-#     # coerce against this by not providing operator/ syntax for supporting this.
 
-################################################################
+def validate(inline: str):
+    return NotImplemented
 
 if __name__ == "__main__":
+    # Going to be doing this kind of parsing before the inline is cast
+    # as an inline object. this avoids having to integrate this validation
+    # within the class body and the code bloat it will cause.
     
-    ### check if validation works for class identiers
-    # print(validate("cls1, cls2, abracadabra"))
-
-    ### check if validation works for class fields- attr, method
-    # print(validate("attr1, attr2 #!@$@", item_type="method"))
-    
-    print(case_check("cls"))
+    basic_validate("class_A : attr1, attr2 : method")
