69de6df6a2b73cb14a35fea5adc1844a9d2165d9
Ben Sehnert <bps53@protonmail.com>
2021-10-18 14:35:53 -0400

    Fixed some formating issues, added the remaining tests so that path_testing module is fully unit tested.
    No implementation so far but hoping to work
    on that later, so we can do integration testing
    and move on to another feature tomorrow

--- tests/testutils/test_path_testing.py
+++ tests/testutils/test_path_testing.py
@@ -1,26 +1,52 @@
 """
 Programmer: Ben Sehnert
-Program: test_inheritance_builder
+Program: test_path_testing
 Date: 1/14/2021
 Software: classgenerator
 Repository-URL: https://github.com/classgenerator
 """
 import unittest
 
+
 class TestPathTesting(unittest.TestCase):
-  """[summary]
-
-  subclassed:
-      unittest ([type]): [description]
-  """
-  def setUp(self) -> None:
-    """set up testing environment"""  
-    return super().setUp()
-
-  def tearDown(self) -> None:
-    """Tear down testing environment"""
-    return super().tearDown()
-  
-  def test_main(self):
-    """[summary]
+    """Unit testing for the module path_testing in the utils package
+    
+    subclassed:
+    unittest ([type]): [description]
     """
+
+    def setUp(self) -> None:
+        """set up testing environment"""  
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        """Tear down testing environment"""
+        return super().tearDown()
+
+    def test_main(self):
+        """[summary]
+        """
+
+
+    #consider a better name for this one? now that we have a regular main method.
+    def test_path_main(self):
+        """[summary]
+        """
+
+    def test_validate_path(self):
+        """[summary]
+        """
+
+    # TODO: what is right case for python fns names? i think isWritable is wrong should be is_writable
+    def test_is_writable(self):
+        """[summary]
+        """
+
+    def test_make_new_folder(self):
+        """[summary]
+        """
+
+    def test_fix_relative_path(self):
+        """[summary]
+        """
+    
