163c2bdf20dc61b1ef7db37fd34f68045162fdda
Ben Sehnert <bps53@protonmail.com>
2021-12-26 18:15:58 -0500

    Unfortunately, not making a lot of headway with this method parsing. Will come back tomorrow with hopefully a clean slate and plenty of problem solving juices.

--- parsing/cleaning.py
+++ parsing/cleaning.py
@@ -28,70 +28,74 @@ def cleanse(items: any):
     return list(map(
         lambda item: item.strip().lower(), items.split(",")))
 
-def cleanse_methods(methods,cleansed):
+def cleanse_methods(methods, parsed):
     """[summary]
 
     Args:
         items (str): comma delimited string listing the methods in a class.
     """
-    if methods.count(","):
-        new_method = methods.split(",")[0]
-        remaining = ",".join(methods.split(",")[1:])
-        if new_method.count("("):
-            components = new_method.split("(")
-            new_method = { 'name' :components[0], 'args'  : components[1].strip(")")}        
-        cleansed += new_method
-        cleanse_methods(remaining, cleansed)
-    return cleansed
+    pass
 
-### TODO: think we can nix these when finished above method.
-def cleanse_regular_methods(items):
+def cleanse_a_method(method):
     """[summary]
 
     Args:
-        items ([type]): [description]
+        method ([type]): [description]
     """
-    regular = []
-    # why is this test needed for regular methods ? (would make an invalid identifier)
-    if items.count(")"):
-        for item in items.split(")"):
-            if item.count("("):
-                regular.append((item.split("(")[0]).split(",")[:-1])
-        for x,y in enumerate(regular):
-            # wut is this? what does it do?
-            if len(y):
-                if y[0] == '':
-                    del y[0]
-            else:
-                # remove empty lists
-                del regular[x]
-        regular = [item for item in regular if item != ['']] 
-        regular = regular[0]
-        return list(map(lambda x: x.strip().lower(), regular))
-    else:
-        return cleanse(items)
+    if method.count("(") and method.count(")"):
+        
 
 
-def cleanse_with_signitures(items):
-    """Modified version of cleanse that deals
-    with the fact that method signitures have ','
-    when there are multiple parameters
 
-    Args:
-        items ([type]): [description]
-    """
-    sigs, signitures = [],[]
-    items = items.split(")")
-    for item in items:
-        if item.count("("):
-            params = item[item.find("("):]
-            name = (item.split("(")[0]).split(",")[-1]
-            sigs.append(f"{name}{params})")
 
-    for sig in sigs:
-        name = sig.split("(")[0]
-        rest = (sig.split("(")[1]).split(")")[0]
-        signitures.append(f"{name.lower().strip()}({rest})")
-    return signitures
+### TODO: think we can nix these when finished above method.
+# def cleanse_regular_methods(items):
+#     """[summary]
+#     Args:
+#         items ([type]): [description]
+#     """
+#     regular = []
+#     # why is this test needed for regular methods ? (would make an invalid identifier)
+#     if items.count(")"):
+#         for item in items.split(")"):
+#             if item.count("("):
+#                 regular.append((item.split("(")[0]).split(",")[:-1])
+#         for x,y in enumerate(regular):
+#             # wut is this? what does it do?
+#             if len(y):
+#                 if y[0] == '':
+#                     del y[0]
+#             else:
+#                 # remove empty 
+#                 del regular[x]
+#         regular = [item for item in regular if item != ['']] 
+#         regular = regular[0]
+#         return list(map(lambda x: x.strip().lower(), regular))
+#     else:
+#         return cleanse(items)
 
-print(cleanse_methods("bisk, froggo(tomato, hat, gun), iceage", []))
+
+# def cleanse_with_signitures(items):
+#     """Modified version of cleanse that deals
+#     with the fact that method signitures have ','
+#     when there are multiple parameters
+
+#     Args:
+#         items ([type]): [description]
+#     """
+#     sigs, signitures = [],[]
+#     items = items.split(")")
+#     for item in items:
+#         if item.count("("):
+#             params = item[item.find("("):]
+#             name = (item.split("(")[0]).split(",")[-1]
+#             sigs.append(f"{name}{params})")
+
+#     for sig in sigs:
+#         name = sig.split("(")[0]
+#         rest = (sig.split("(")[1]).split(")")[0]
+#         signitures.append(f"{name.lower().strip()}({rest})")
+#     return signitures
+
+# very curious output...
+print(cleanse_methods("bisk, froggo(tomato, hat, gun), iceage",[]))
